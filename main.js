58ce75a1cbedec2b1916660f22d07628735bfcf9
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-18 15:55:23 +0200

    js: edit

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let name = prompt("Add your name");
+console.log(name);
 
-let username = prompt("Add your username");
+let surname = prompt("Add your username");
+console.log(surname);
 
 let color = prompt("Add your favorite color");
+console.log(color);
+
+let password = name + surname + color + "21";
+
+const passwordGenerator = `
+${password}`;
+
+document.getElementById("password-generator").innerHTML = passwordGenerator;
